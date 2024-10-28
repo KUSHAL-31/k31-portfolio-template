f2946ffe8f90cf87b056771bd1f27ad160eb9d75
KUSHAL-31 <s.kushal31@gmail.com>
2024-10-28 16:11:24 +0530

    banner and contact me images updated

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -1,16 +1,18 @@
 import React, { useContext } from 'react'
-import { bannerImg } from "../../assets/index";
 import { PortfolioContext } from '../../contexts/PortfolioContext';
+import { defaultPerson } from "../../assets";
 
 const RightBanner = () => {
 
   const { portfolioData } = useContext(PortfolioContext);
+  const imageUrl = portfolioData?.userMetaData?.images[0].url || defaultPerson;
+
 
   return (
     <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
       <img
-        className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
-        src={bannerImg}
+        className="w-[300px] h-[300px] lgl:w-[450px] lgl:h-[450px] z-10"
+        src={imageUrl}
         alt="bannerImg"
       />
       <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center"></div>
--- src/components/contact/ContactLeft.js
+++ src/components/contact/ContactLeft.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import { defaultPerson } from "../../assets";
 
 const ContactLeft = ({ portfolioData }) => {
+
+  const imageUrl = portfolioData?.userMetaData?.images.length === 2 ? portfolioData?.userMetaData?.images[1].url : portfolioData?.userMetaData?.images.length === 1 ? portfolioData?.userMetaData?.images[0].url : defaultPerson;
+
   return (
     <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-8 justify-center">
       <img
         className="w-full h-64 rounded-lg mb-2 object-contain"
-        src={portfolioData.user.avatar}
+        src={imageUrl}
         alt="contactImg"
       />
       <div className="flex flex-col gap-4">
